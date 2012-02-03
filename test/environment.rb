7a060ec3142dd547219f7136c5492c88fd6b6ef7
Salman Ahmad <salman@salmanahmad.com>
2012-02-02 21:57:44 -0500

    Adding the bulk of the grammar back to Dog from Citrus.

--- test/environment.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-
-require 'rubygems'
-require 'bundler/setup'
-require File.expand_path('../../lib/man_reduce.rb', __FILE__)
---
+++ test/test_helper.rb
@@ -0,0 +1,17 @@
+#
+# Copyright 2011 by Salman Ahmad (salman@salmanahmad.com).
+# All rights reserved.
+#
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as the
+# above copyright notice is included.
+#
+
+require 'rubygems'
+require 'test/unit'
+
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
+
+require 'rubygems'
+require 'bundler/setup'
+require File.expand_path('../../lib/dog.rb', __FILE__)
