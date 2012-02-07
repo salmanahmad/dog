30030ecd8f367b61e9d527c95d332e0e370fd597
Salman Ahmad <salman@salmanahmad.com>
2012-02-07 14:33:06 -0500

    Adding placeholders for tests to come.

---
+++ test/unit/parser/compute.rb
@@ -0,0 +1,24 @@
+#
+# Copyright 2011 by Salman Ahmad (salman@salmanahmad.com).
+# All rights reserved.
+#
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as the
+# above copyright notice is included.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'test_helper.rb'))
+
+class ComputeTest < Test::Unit::TestCase
+  
+  def setup
+    @parser = Dog::Parser.new
+    @parser.parser.root = :for
+  end
+  
+  def test_simple
+    
+  end
+  
+  
+end
