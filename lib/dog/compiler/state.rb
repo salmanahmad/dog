54c6aff1246d1e10666c9b3da2503ca05c8efacd
Salman Ahmad <salman@salmanahmad.com>
2012-02-15 23:06:54 -0500

    Adding the new states for the runtime.

--- lib/dog/compiler/state.rb
+++ lib/dog/compiler/state.rb
@@ -10,23 +10,20 @@
 module Dog
   
   class State
-    
     attr_accessor :parent
     attr_accessor :children
-    attr_accessor :operation
     
     def initialize
       @parent = nil
       @children = []
-      @variable_dependencies = []
-      @variable_output = nil
-      @operation = nil
     end
     
     def run
-      # TODO
+      raise "Run on state was called."
     end
-    
+  end
+  
+  class ProgramState < State
     def to_bark
       Marshal.dump(self)
     end
@@ -36,9 +33,50 @@ module Dog
       raise "Error" if state.class != self
       return state
     rescue
-      raise "Could not load state machine from bark."
+      raise "Could not load program from bark."
     end
     
+    def run
+      
+    end
+  end
+  
+  class OperationState < State
+    attr_accessor :operation
+    
+    def run
+      
+    end
+  end
+  
+  class OnState < State
+    attr_accessor :dependency
+    
+    def run
+      
+    end
+  end
+  
+  class ForState < State
+    attr_accessor :enumerable
+    
+    def run
+      
+    end
+  end
+  
+  class IfState < State
+    def run
+      
+    end
+  end
+  
+  class ConditionState < State
+    attr_accessor :condition
+    
+    def run
+      
+    end
   end
   
 end
