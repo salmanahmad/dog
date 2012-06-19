9b6e446749f4c100115b6bd736c7096576c51dad
Salman Ahmad <salman@salmanahmad.com>
2012-06-19 19:16:36 -0400

    Progress restructuring the grammar.

---
+++ test/unit/parser/assignment.rb
@@ -0,0 +1,47 @@
+#
+# Copyright 2011 by Salman Ahmad (salman@salmanahmad.com).
+# All rights reserved.
+#
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as the
+# above copyright notice is included.
+#
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'test_helper.rb'))
+
+class ParserTests::AssignmentTest < Test::Unit::TestCase
+  
+  def setup
+    @parser = Dog::Parser.new
+    @parser.parser.root = :assignment
+  end
+  
+  def test_assignment
+    
+    @parser.parse("foo = (5) UNION (5 - 5) / 5")
+    
+    #@parser.parse("i = ASK ME VIA email TO rank")
+
+    @parser.parse("i = 0")
+    @parser.parse("i = 1")
+    @parser.parse("i = -1")
+    @parser.parse("i = 1.1")
+    @parser.parse("i = -1.1")
+    @parser.parse("i = true")
+    @parser.parse("i = false")
+    @parser.parse("i = [1,2,3]")
+    @parser.parse('i = "Foo bar"')
+    @parser.parse("i = 'Foo bar'")
+    @parser.parse("i = {'key':'value'}")
+
+    @parser.parse("i.j = 8") 
+    @parser.parse("i[i] = {'key':'value'}")
+    @parser.parse("i[0] = {'key':'value'}")
+    @parser.parse("i['string'] = {'key':'value'}")
+    @parser.parse("i[j[k]][l] = {'key':'value'}")
+
+    @parser.parse("i.j.k.l = {'key':'value'}")
+
+    @parser.parse("i's j's k = {'key':'value'}")
+  end
+end
--- test/unit/parser/expression.rb
+++ test/unit/parser/expression.rb
@@ -16,32 +16,6 @@ class ParserTests::ExpressionTest < Test::Unit::TestCase
     @parser.parser.root = :expression
   end
   
-  def test_assignment
-    @parser.parse("i = ASK ME VIA email TO rank")
-    
-    @parser.parse("i = 0")
-    @parser.parse("i = 1")
-    @parser.parse("i = -1")
-    @parser.parse("i = 1.1")
-    @parser.parse("i = -1.1")
-    @parser.parse("i = true")
-    @parser.parse("i = false")
-    @parser.parse("i = [1,2,3]")
-    @parser.parse('i = "Foo bar"')
-    @parser.parse("i = 'Foo bar'")
-    @parser.parse("i = {'key':'value'}")
-    
-    @parser.parse("i.j = 8") 
-    @parser.parse("i[i] = {'key':'value'}")
-    @parser.parse("i[0] = {'key':'value'}")
-    @parser.parse("i['string'] = {'key':'value'}")
-    @parser.parse("i[j[k]][l] = {'key':'value'}")
-    
-    @parser.parse("i.j.k.l = {'key':'value'}")
-    
-    @parser.parse("i's j's k = {'key':'value'}")
-  end
-  
   def test_operation
     @parser.parse("NOT TRUE")
     @parser.parse("5 + 5")
@@ -59,7 +33,6 @@ class ParserTests::ExpressionTest < Test::Unit::TestCase
     @parser.parse("5 / 2 + 5")
     @parser.parse("(5) + (5 + 5) + 5")
     @parser.parse("(5) UNION (5 - 5) / 5")
-    @parser.parse("foo = (5) UNION (5 - 5) / 5")
   end
   
   def test_access
@@ -75,9 +48,6 @@ class ParserTests::ExpressionTest < Test::Unit::TestCase
     @parser.parse("foo[bar[bar]]")
     @parser.parse("(foo[bar])[baz]")
     @parser.parse("foo[bar][baz[foo][bar]][poo]")
-    
-    @parser.parse("foo's bar's baz")
-    @parser.parse("foo's bars' baz")
   end
   
 end
