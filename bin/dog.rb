bc04cb5a8bcf7fd59c0b1871a8675d17820d1618
Salman Ahmad <salman@salmanahmad.com>
2012-06-26 21:24:47 -0400

    Scaffolding the dog command line utility

--- bin/dog.rb
+++ bin/dog.rb
@@ -8,6 +8,21 @@
 # above copyright notice is included.
 #
 
+
+#
+# General Compilation Pipeline: 
+#
+# Parse the dog code into an AST (called a bark)
+# => bark = Dog::Parser.parse(dog_code)
+#
+# Compute the AST into a vm byte code (called bite code)
+# => bite = Dog::Compiler.compile(bark)
+#
+# Execute the byte code. This may save state as an execution graph (called a track)
+# => Dog::Runtime.run(bite)
+#
+
+
 #ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
 
 require 'rubygems'
@@ -15,43 +30,173 @@ require 'bundler/setup'
 require File.join(File.dirname(__FILE__), '../lib/dog.rb')
 
 
-command = ARGV.first || ""
-
-case command.downcase
-when "init"
-
-when "shell"
+class Command
   
-when "compile"
-
-when "run"
+  class << self
+    
+    attr_accessor :registered_commands
+    
+    def register(command)
+      self.registered_commands ||= {}
+      self.registered_commands[command.new.name] = command.new
+    end
+    
+    def find_command_by_name(name)
+      object = self.registered_commands[name]
+      if object then
+        object
+      else
+        nil
+      end
+    end
+    
+    def run(args)
+      self.new.run(args)
+    end
+    
+  end
   
-when "help"
+  def name
+    self.class.name.downcase
+  end
   
-else
-  puts "Dog #{Dog::VERSION::STRING} (#{Dog::VERSION::CODENAME})"
-  puts
-  puts "Usage: dog COMMAND [command-specific-arguments]"
-  puts
-  puts "List of commands, type \"dog help COMMAND\" for more details:"
-  puts
-  puts "  init     # Create a new Dog application directory"
-  puts "  compile  # Compile a Dog source file or application"
-  puts "  run      # Execute a Dog source file or application"
-  puts "  shell    # Start a shell session with a running Dog application"
-  puts
+  def usage
+    puts "Dog #{Dog::VERSION::STRING} (#{Dog::VERSION::CODENAME})"
+  end
+  
+  def run(args)
+    command = self.class.find_command_by_name((args.first || "").downcase)
+    
+    if command then
+      args.shift
+      command.run(args)
+    else
+      Help.new.usage
+    end
+  end
 end
+
+class Init < Command
+  Command.register(self)
   
+  def description
+    "Create a new Dog application directory"
+  end
+  
+  def usage
+    
+  end
+  
+  def run(args)
+    
+  end  
+end
+
+class Compile < Command
+  Command.register(self)
+  
+  def description
+    "Compile a Dog source file or application"
+  end
+  
+  def usage
+    
+  end
+  
+  def run(args)
+    
+  end
+end
+
+class Run < Command
+  Command.register(self)
+  
+  def description
+    "Execute a Dog source file or application"
+  end
+  
+  def usage
+    
+  end
+  
+  def run(args)
+    
+  end
+end
+
+class Shell < Command
+  Command.register(self)
+  
+  def description
+    "Start a shell session with a running Dog application"
+  end
+  
+  def usage
+    
+  end
+  
+  def run(args)
+    
+  end
+end
+
+class Help < Command
+  Command.register(self)
+  
+  def description
+    "Show the help page for a command"
+  end
+  
+  def usage
+    super
+    puts
+    puts "Usage: dog COMMAND [command-specific-arguments]"
+    puts
+    puts "List of commands, type \"dog help COMMAND\" for more details:"
+    puts
+    
+    max_length = 0
+    
+    for key, value in Command.registered_commands do
+      max_length = [max_length, key.length].max
+    end
+    
+    for key, value in Command.registered_commands do
+      remaining = max_length - key.length
+      remaining += 2
+      
+      print "  #{key}"
+      remaining.times { print " " }
+      print "# "
+      print value.description
+      print "\n"
+    end
+    
+    puts
+    
+  end
+  
+  def run(args)
+    command = Command.find_command_by_name((args.first || "").downcase)
+    
+    if command then
+      command.usage
+    else
+      self.usage
+    end
+  end
+end
+
+
+Command.run(ARGV.clone)
+
+
 
 
 
 __END__
 
 
-if ARGV.empty? then
-  puts "error: no input file provided"
-  exit
-end
 
 dog_file = ARGV.last
 dog_code = File.open(dog_file).read
@@ -74,7 +219,4 @@ bite = Dog::Compiler.compile(bark)
 bite_code_file = File.open(bite_code_filename, "w")
 bite_code_file.write(Base64.encode64(bite.to_hash.inspect))
 
-# Execute the byte code. This may save state 
-# as an execution graph (called a track)
-#Dog::Runtime.run(bite)
 
