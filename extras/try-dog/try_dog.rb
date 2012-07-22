9b80d499bd343574ced5fdaa6e31c933f23ffb0d
Salman Ahmad <salman@salmanahmad.com>
2012-07-22 18:29:34 -0400

    Updating the shell to work with try-dog.

--- extras/try-dog/try_dog.rb
+++ extras/try-dog/try_dog.rb
@@ -1,13 +1,26 @@
 require 'sinatra'
 
+helpers do
+  def dog
+    File.join(File.dirname(__FILE__), "../../bin/dog")
+  end
+end
+
 get '/' do
   redirect '/index.html'
 end
 
-get '/evaluate/:id' do |id|
+get '/evaluate' do
   
+  track = params["track"]
+  statement = params["statement"]
+  if track then
+    `#{dog} shell -t #{track} "#{statement}"`
+  else
+    `#{dog} shell "#{statement}"`
+  end
 end
 
-get '/version' do |id|
-  
+get '/version' do
+  `#{dog} version`
 end
