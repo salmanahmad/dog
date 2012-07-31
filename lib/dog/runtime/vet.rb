1a0196887bb76d94ba4d13b3584469e1952c3f7c
Rizwan Ahmad <rizwanahmad93@gmail.com>
2012-07-31 18:09:06 -0400

    Added the login page.

--- lib/dog/runtime/vet.rb
+++ lib/dog/runtime/vet.rb
@@ -20,12 +20,48 @@ module Dog
 
         Dir.foreach(File.join(File.dirname(__FILE__), "vet")) do |f|
           full_path = File.join(File.dirname(__FILE__), "vet", f)
-          unless [".", ".."].include? f then
+          unless [".", "..", "index.html", "login.html"].include? f then
             route = prefix + "/" + f
             get route do
               send_file full_path
             end
           end
+        end
+        
+        get prefix + "/index.html" do
+          if(session["login"] == true)
+            file = File.join(File.dirname(__FILE__), "vet", "index.html")
+            file = File.open(file)
+            return file.read
+#            send_file File.join(File.dirname(__FILE__), "vet", "index.html")
+          else
+            redirect prefix + "/login.html"
+          end
+        end
+
+        get prefix + "/login.html" do
+          if(session["login"] == true)
+            redirect prefix + "/index.html"
+          else
+            send_file File.join(File.dirname(__FILE__), "vet", "login.html")
+          end
+        end
+
+        post prefix + "/login.html" do
+          @username = params["username-field"]
+          @password = params["password-field"]
+          if(@username == "username" && @password == "password")
+            session["login"] = true
+            redirect prefix + "/index.html"
+          else
+            session["login"] = false
+            redirect prefix + "/login.html"
+          end
+        end
+        
+        post prefix + "/logout.html" do
+          session["login"] = false
+          redirect prefix + "/login.html"
         end
       end
     end
