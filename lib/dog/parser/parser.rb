e657b11546660235e3c1aaf5872e49b83fee216e
Salman Ahmad <salman@salmanahmad.com>
2012-08-09 13:26:51 -0400

    Copying the old grammar file and renaming compile to transform for nodes.:

--- lib/dog/parser/parser.rb
+++ lib/dog/parser/parser.rb
@@ -8,55 +8,54 @@
 #
 
 module Dog
-  
+
   class ParseError < RuntimeError
     attr_accessor :line
     attr_accessor :column
     attr_accessor :failure_reason
   end
-  
+
   class Parser
-    
+
     class << self
       attr_accessor :grammar_loaded
     end
-    
+
     def self.parse(program, filename = "")
       parser = self.new
       parser.parse(program, filename)
     end
-    
+
     attr_accessor :parser
-    
+
     def initialize
       unless Parser.grammar_loaded then
         Treetop.load(File.expand_path(File.join(File.dirname(__FILE__), 'grammar.treetop')))
         Parser.grammar_loaded = true
       end
-      
+
       @parser = DogParser.new
       @should_clean_tree = false
     end
-    
+
     def parse(program, filename = "")
       filename = File.expand_path(filename) unless filename.strip.length == 0
-      
+
       tree = @parser.parse(program)
       
       if(tree.nil?)
         error = ParseError.new("Parsing error: (#{filename}:#{@parser.failure_line}).\n\n#{@parser.failure_reason.inspect}")
-        
+
         error.line = @parser.failure_line
         error.column = @parser.failure_column
         error.failure_reason = @parser.failure_reason
-        
-        
+
         raise error
       end
-      
-      return tree.compile
+
+      return tree.transform
     end
-    
+
   end
-  
+
 end
