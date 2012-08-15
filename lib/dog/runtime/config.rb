9780700d58ea86f134e24a22da4be3bf8cdc6d2a
Zahan Malkani <zahanm@gmail.com>
2012-08-15 18:32:18 -0400

    fix config file reading, move athena stuff out

--- lib/dog/runtime/config.rb
+++ lib/dog/runtime/config.rb
@@ -8,56 +8,54 @@
 #
 
 module Dog
-  
+
   class Config
-    
+
     class << self
-      
+
       def initialize(config_file = nil, config = {})
         return if @initialized
         @initialized = true
-        
+
         database_default = Runtime.bundle.startup_package
         database_default = File.basename(Runtime.bundle_filename, ".bundle") if database_default == "" rescue ""
-        
+
         if database_default == "" then
           raise "I could not start Dog because I did not have a database name I could use"
         end
-        
+
         @config ||= {
           'port' => 4242,
           'dog_prefix' => '/dog',
-          'database' => database_default,
-          'facebook_app_id' => '433590860017315',
-          'facebook_app_secret' => '31cb6a2193855a179f3df40274b4ca27'
+          'database' => database_default
         }
-        
-        config_file ||= File.join(Runtime.bundle_directory, "config.json")
-        
+
+        config_file = File.join(Runtime.bundle_directory, "config.json") unless config_file and config_file.length > 0
+
         @config.merge!(JSON.parse(File.open(config_file).read)) rescue nil
         @config.merge!(config)
-        
+
         if @config["dog_prefix"][-1,1] == "/" then
           @config["dog_prefix"].chop!
         end
-        
+
       end
-      
+
       def reset
         @initialized = false
         @config = {}
       end
-      
+
       def set(key, value)
         @config[key] = value
       end
-      
+
       def get(key)
         @config[key]
       end
-      
+
     end
-    
+
   end
-  
+
 end
