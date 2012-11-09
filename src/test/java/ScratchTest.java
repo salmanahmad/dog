4306280ba726733b16f0d0737b89ca2470121b38
Salman Ahmad <salman@salmanahmad.com>
2012-11-09 00:57:03 -0500

    Cleaning up my scratch file.

--- src/test/java/ScratchTest.java
+++ src/test/java/ScratchTest.java
@@ -1,45 +1,42 @@
 
-import junit.framework.*; 
-import org.reflections.*;
-import org.reflections.util.*;
-import org.reflections.scanners.*;
-
 import java.util.*;
+import org.junit.*; 
 
-public class ScratchTest extends TestCase {
-  public void test() {
+public class ScratchTest {
     
-    //dog.lang.Runtime.run();
-    
+    @Test
+    public void test() {
+
+        //dog.lang.Runtime.run();
+
     /*
-    Reflections reflections = new Reflections(new ConfigurationBuilder()
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
             .setScanners(new SubTypesScanner(false), new ResourcesScanner())
             .setUrls(ClasspathHelper.forPackage("dog")));
             //.filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("dog.lang"))));
 
-      
-      // I shoudl consider using getTypesAnnotatedWith instead
-    
-    Reflections reflections = new Reflections("dog");
-    
-    Set<Class<? extends dog.lang.Value>> allClasses = reflections.getSubTypesOf(dog.lang.Value.class);
-    
-    System.out.println(allClasses.size());
-    System.out.println("\n\n");
-    System.out.println(allClasses.toString());
-    
-    
-    Package[] packages = Package.getPackages();
-    for (int i = 0; i < packages.length; i++) {
-      String name = packages[i].getName();
-      
-      //System.out.println(name);
-      if(name.equals("dog.lang")) {
-        //System.out.println("\n\n-- HERE -- \n");
-      }
-    }
-        */
-    
-  }
 
+      // I shoudl consider using getTypesAnnotatedWith instead
+
+        Reflections reflections = new Reflections("dog");
+
+        Set<Class<? extends dog.lang.Value>> allClasses = reflections.getSubTypesOf(dog.lang.Value.class);
+
+        System.out.println(allClasses.size());
+        System.out.println("\n\n");
+        System.out.println(allClasses.toString());
+
+
+        Package[] packages = Package.getPackages();
+        for (int i = 0; i < packages.length; i++) {
+            String name = packages[i].getName();
+
+            //System.out.println(name);
+            if(name.equals("dog.lang")) {
+                //System.out.println("\n\n-- HERE -- \n");
+            }
+        }
+        */
+
+    }
 }
