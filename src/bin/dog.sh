a735301fd918c720bb746febde8ffef78b6de161
Salman Ahmad <salman@salmanahmad.com>
2012-11-16 12:57:15 -0500

    Setting up the dog shell scripts and command structure. Figuring out how to use antrun for custom deploy tasks.

---
+++ src/bin/dog.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# Copyright 2011 by Salman Ahmad (salman@salmanahmad.com).
+# All rights reserved.
+#
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as the
+# above copyright notice is included.
+#
+
+
+# resolve links - $0 may be a soft-link
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`/"$link"
+    fi
+done
+
+# TODO - I need to switch over to the jar-with-dependencies at some point.
+
+DIR_NAME=`dirname "$PRG"`
+DEP_PATH="$DIR_NAME/../../target/dependency/*"
+
+JAR_NAME="dog.jar"
+JAR_PATH="$DIR_NAME/../../target/lib/$JAR_NAME"
+
+CLASSPATH="$JAR_PATH:$DEP_PATH"
+COMMAND_NAME="dog.lang.commands.Main"
+
+exec java -classpath "$CLASSPATH" "$COMMAND_NAME"  "$@"
--- src/main/java/dog/commands/Main.java
+++ src/main/java/dog/commands/Main.java
@@ -1,1 +1,26 @@
+/*
+ *
+ *  Copyright 2012 by Salman Ahmad (salman@salmanahmad.com).
+ *  All rights reserved.
+ *
+ *  Permission is granted for use, copying, modification, distribution,
+ *  and distribution of modified versions of this work as long as the
+ *  above copyright notice is included.
+ *
+ */
+
+package dog.lang.commands;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Bark bark!");
+		System.out.println(args);
+	}
+}
+
+
+
 
