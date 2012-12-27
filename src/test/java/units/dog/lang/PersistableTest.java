0ccd583d04eda720919cfc49cefd7a2da24da497
Salman Ahmad <salman@salmanahmad.com>
2012-12-27 10:16:45 -0700

    Adding the template for the persistable test cases.

---
+++ src/test/java/units/dog/lang/PersistableTest.java
@@ -0,0 +1,45 @@
+/*
+ *
+ *  Copyright 2012 by Salman Ahmad (salman@salmanahmad.com).
+ *  All rights reserved.
+ *
+ *  Permission is granted for use, copying, modification, distribution,
+ *  and distribution of modified versions of this work as long as the
+ *  above copyright notice is included.
+ *
+ */
+
+
+import org.junit.*;
+import java.util.*;
+
+import dog.lang.NumberValue;
+import dog.lang.StringValue;
+
+public class PersistableTest {
+    
+    @Test
+    public void testNumber() {
+        
+    }
+
+    @Test
+    public void testBoolean() {
+        
+    }
+
+    @Test
+    public void testString() {
+        
+    }
+
+    @Test
+    public void testNull() {
+        
+    }
+
+    @Test
+    public void testStructure() {
+        
+    }
+}
