6741be6ae78c476ba87957f293035534bcfb16d2
Samarth Mohan <smohan94@mit.edu>
2013-01-15 20:40:36 -0500

    Changed dog.sh for debug

--- src/bin/debug/dog.sh
+++ src/bin/debug/dog.sh
@@ -24,10 +24,10 @@ done
 
 # TODO - I need to switch over to the jar-with-dependencies at some point.
 DIR_NAME=`dirname "$PRG"`
-DEP_PATH="$DIR_NAME/../lib/dependency/*"
+DEP_PATH="$DIR_NAME/../../../target/dependency/*"
 
 JAR_NAME="dog.jar"
-JAR_PATH="$DIR_NAME/../lib/$JAR_NAME"
+JAR_PATH="$DIR_NAME/../../../target/lib/$JAR_NAME"
 
 CLASSPATH="$JAR_PATH:$DEP_PATH"
 COMMAND_NAME="dog.commands.Main"
