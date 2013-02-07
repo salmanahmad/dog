79682d745f95a73cf1e8602c734e66c493bc3e5d
Salman Ahmad <salman@salmanahmad.com>
2013-02-07 08:54:14 -0500

    Adding method stubs for the collection API.

---
+++ src/main/java/dog/packages/collection/Remove.java
@@ -0,0 +1,34 @@
+
+package dog.packages.dog;
+
+import dog.lang.Value;
+import dog.lang.FalseValue;
+import dog.lang.TrueValue;
+import dog.lang.StringValue;
+import dog.lang.Function;
+import dog.lang.Signal;
+import dog.lang.StackFrame;
+import dog.lang.annotation.Symbol;
+
+@Symbol("collection.remove:from:")
+public class Add extends Function {
+
+	public int getRegisterCount() {
+		return 1;
+	}
+
+	public int getVariableCount() {
+		return 1;
+	}
+
+	public Signal resume(StackFrame frame) {
+		Value returnValue = new TrueValue();
+		Value value = frame.variables[0];
+		
+		frame.returnRegister = 0;
+		frame.registers[0] = returnValue;
+		
+		return new Signal(Signal.Type.RETURN);
+	}
+}
+
